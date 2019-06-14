7007ee37f35a1fbf52220e4142721113c9978ed1
shaktigurung <mr.shakti.gurung@gmail.com>
2019-06-14 15:17:51 +1000

    Initial setup and adminLte integrated

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Menu from './Menu';
+import Footer from './Footer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Menu />
+        <Footer />
+      </div>
+    )
+  }
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="main-footer">
+            <div className="pull-right hidden-xs">
+            <b>Version</b> 2.4.0
+            </div>
+            <strong>Copyright © 2019 <a href="https://adminlte.io"> Piiq Digital </a>.</strong> All rights
+            reserved.
+        </footer>
+      </div>
+
+    )
+  }
+}
