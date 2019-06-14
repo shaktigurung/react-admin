d7b2dbc6763a1044cb94f47cdd65ccd9434d192d
shaktigurung <mr.shakti.gurung@gmail.com>
2019-06-14 15:50:57 +1000

    table integrated

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Menu from './Menu';
+import Content from './Content';
 import Footer from './Footer';
 
 export default class App extends Component {
@@ -9,6 +10,7 @@ export default class App extends Component {
       <div>
         <Header />
         <Menu />
+        <Content />
         <Footer />
       </div>
     )
